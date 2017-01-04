56740ae030639b209a3e8d32cb94e70883187411
xiaozhuai <798047000@qq.com>
2017-01-04 15:03:57 +0800

    add all.h & all.cpp

---
+++ src/cxxurl_all.cpp
@@ -0,0 +1,18 @@
+/**
+ * @author : xiaozhuai
+ * @date   : 17/1/4
+ */
+
+#ifndef CXXURL_CXXURL_ALL_CPP_H
+#define CXXURL_CXXURL_ALL_CPP_H
+
+#include "Form.cpp"
+#include "Header.cpp"
+#include "MultipartForm.cpp"
+#include "RawForm.cpp"
+#include "Request.cpp"
+#include "RequestBuilder.cpp"
+#include "SimpleForm.cpp"
+#include "UrlEncode.cpp"
+
+#endif //CXXURL_CXXURL_ALL_CPP_H
---
+++ src/cxxurl_all.h
@@ -0,0 +1,19 @@
+/**
+ * @author : xiaozhuai
+ * @date   : 17/1/3
+ */
+
+#ifndef CXXURL_CXXURL_ALL_H
+#define CXXURL_CXXURL_ALL_H
+
+#include "Form.h"
+#include "Header.h"
+#include "MultipartForm.h"
+#include "RawForm.h"
+#include "Request.h"
+#include "RequestBuilder.h"
+#include "SimpleForm.h"
+#include "UrlEncode.h"
+#include "version.h"
+
+#endif //CXXURL_CXXURL_ALL_H
