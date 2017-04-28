8666ca57cd0a30157678fcc132fe3219b3a5186b
xiaozhuai <798047000@qq.com>
2017-04-28 16:31:22 +0800

    update ez_test

--- tests/ez_test.h
+++ tests/ez_test.h
@@ -1,26 +1,42 @@
-//
-// Created by xiaozhuai on 17/4/28.
-//
+/*
+ * Created by xiaozhuai on 17/4/28.
+ * Email xiaozhuai7@gmail.com
+ *
+ * MIT License
+ *
+ * Copyright (c) 2017 xiaozhuai, Weihang Ding
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
 
 #ifndef EZ_TEST_H
 #define EZ_TEST_H
 
-#include <stdio.h>
 #include <stdlib.h>
-
-#define EZ_ASSERT(X)                                                                                \
-        if(!(X)) {                                                                                  \
-            printf("EZTest Failed %s:%d %s\n", __FILE__, __LINE__, #X);                             \
-            exit(1);                                                                                \
-        }
-
-#define EZ_ASSERT_EQUAL(a, b)                                                                       \
-        EZ_ASSERT(((a)==(b)))
-
-#define EZ_ASSERT_NOT_EQUAL(a, b)                                                                   \
-        EZ_ASSERT(((a)!=(b)))
-
-#define EZ_TEST                                                                                     \
-        int main()
+#include <stdio.h>
+#define EZ_ASSERT(X)                    if(!(X)) {                                                      \
+                                            printf("EZTest Failed %s:%d %s\n", __FILE__, __LINE__, #X); \
+                                            exit(1);                                                    \
+                                        }
+#define EZ_ASSERT_EQUAL(a, b)           EZ_ASSERT(((a)==(b)))
+#define EZ_ASSERT_NOT_EQUAL(a, b)       EZ_ASSERT(((a)!=(b)))
+#define EZ_TEST                         int main()
 
 #endif //EZ_TEST_H
